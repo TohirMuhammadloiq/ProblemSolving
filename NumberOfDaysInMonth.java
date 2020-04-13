a160406883084bacee4608230fa6956fe15f440d
Java_projects <tokhirkhuja6499@mail.ru>
2020-04-13 18:25:53 +0300

    Itis helped to find size of month with easily

--- NumberOfDaysInMonth.java
+++ NumberOfDaysInMonth.java
@@ -1,4 +1,68 @@
 package com.muhammadloiq;
 
 public class NumberOfDaysInMonth {
+    public static boolean isLeapYear(int year)
+    {
+        if((year<=0)||(year>9999))
+        {
+            return false;
+        }
+        else
+        {
+
+            if(year % 4 == 0)
+            {
+                if( year % 100 == 0)
+                {
+                    if ( year % 400 == 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public static int getDaysInMonth(int month,int year)
+    {
+        if((month<1||month>12)||(year < 1 || year > 9999))
+        {
+            return -1;
+        }
+        else
+        {
+            switch(month)
+            {
+                case 2:
+                    if(isLeapYear(year))
+                    {
+                        return 29;
+                    }
+                    else
+                    {
+                        return 28;
+                    }
+                case 4: case 6: case 9: case 11:
+                return 30;
+                default:
+                    return 31;
+            }
+        }
+
+
+    }
+
 }
+
