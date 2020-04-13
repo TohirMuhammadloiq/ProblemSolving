1e3d3721c390e2319bbbd12f8a188c68f1048b14
Java_projects <tokhirkhuja6499@mail.ru>
2020-04-13 16:08:08 +0300

    this class solves some problems

---
+++ ProblemSolving.java
@@ -0,0 +1,32 @@
+package com.muhammadloiq;
+public class ProblemSolving{
+
+
+        public int countPrimeNumbersFromGivenIntegers(int starting, int ending){
+            int count = 0;
+
+            for(int i = starting; i<ending; i++){
+
+                if(this.isPrime(i)){
+                    System.out.println("Prime number is " + i);
+                    count++;
+                }
+
+            }
+            return count;
+        }
+
+        public boolean isPrime(int n)
+        {
+            if(n<=1) {
+                return false;
+            }
+            for(int i=2;i<n;i++) {
+                if(n%i==0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+}
