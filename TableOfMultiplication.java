6a7cdae08c5c574960b70a4dee5460a2de2635fe
Java_projects <tokhirkhuja6499@mail.ru>
2020-07-20 10:35:50 +0300

    It is calculating multiplication

---
+++ TableOfMultiplication.java
@@ -0,0 +1,9 @@
+package com.muhammadloiq;
+
+public class TableOfMultiplication {
+    public static void Multiplication(int number) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " * " + i + " = " + number * i);
+        }
+    }
+}
